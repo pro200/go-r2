1f9067e9fa971e61fc0fed4bed459ddb956d56c8
pro200 <pro200@gmail.com>
2025-09-22 15:34:03 +0900

    update

--- r2_test.go
+++ r2_test.go
@@ -4,33 +4,15 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/pro200/go-env"
 	"github.com/pro200/go-r2"
 )
 
 func TestR2(t *testing.T) {
-	err := env.Init()
-	if err != nil {
-		t.Error(err)
-	}
-
-	accountId, err := env.Get("ACCOUNT_ID")
-	if err != nil {
-		t.Error(err)
-	}
-	accessKeyId, err := env.Get("ACCESS_KEY_ID")
-	if err != nil {
-		t.Error(err)
-	}
-	secretAccessKey, err := env.Get("SECRET_ACCESS_KEY")
-	if err != nil {
-		t.Error(err)
-	}
-
+	// read only token: R2 Dev - 4fe41a62e8
 	r2.Init(r2.Config{
-		AccountId:       accountId,
-		AccessKeyID:     accessKeyId,
-		SecretAccessKey: secretAccessKey,
+		AccountId:       "882b4dd326c8b297dd43e5df32f08b6d",
+		AccessKeyID:     "4fe41a62e8e71d9475c6ad46a3b8bb3c",
+		SecretAccessKey: "33c2eb7deac4d187e1d3d2a51cf0b6620a66107cec95a6d9abdcaed059e2dacd",
 	})
 
 	r, _, err := r2.List("dev", "", 10)
